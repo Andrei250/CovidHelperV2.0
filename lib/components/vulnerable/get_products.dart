160a2c6c25d0222963aad151ffddb27506a88aa7
Andrei250 <andrei_dumitrescu2000@yahoo.ro>
2020-08-29 18:24:15 +0300

    Added ordering page

---
+++ lib/components/vulnerable/get_products.dart
@@ -0,0 +1,127 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:covidhelper_v2/utils/app_theme.dart';
+import 'package:covidhelper_v2/utils/pics.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class GetProducts extends StatefulWidget {
+  @override
+  _GetProductsState createState() => _GetProductsState();
+}
+
+class _GetProductsState extends State<GetProducts> {
+
+
+
+  AppBar interfaceAppBar() {
+    return AppBar(
+      elevation:0,
+      backgroundColor: Colors.white,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/back_arrow.svg",
+          color: Colors.black,
+        ),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'Covidhelper',
+            style: eTitle,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 40.0,
+                height: 40.0,
+                child: Center(
+                    child: Text(
+                      's',
+//                          child: Text(getInitials(widget.name),
+                      style: eStreet,
+                    )),
+                decoration: new BoxDecoration(
+                  color: AppTheme.lightAccent,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                width: 40.0,
+                height: 40.0,
+                child: Center(
+                  child: IconButton(
+                    icon: Icon(Icons.shopping_cart_rounded),
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: interfaceAppBar(),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: 54,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: Colors.grey.withOpacity(0.23),
+                  )
+                ]
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                      hintText: "Cauta produse",
+                      hintStyle: TextStyle(
+                          color: Colors.grey.withOpacity(0.5)
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SvgPicture.asset("assets/icons/search.svg"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print(Firestore.instance.collection('vendor').snapshots()
+              .map((snapshot) => snapshot.documents));
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
